dcbe97302da8ed5db9d852d5d8b891a85427db29
Shubham Singh <shubhamsinghcodex@gmail.com>
2023-02-13 14:19:38 +0530

    silly update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -12,12 +12,14 @@ const About = (props) => {
             <span className="space"></span>I am Shubham Singh, B.Tech. CSE
             student at LNCT Jabalpur College. <br />
             <br />
-            I have learned Core Java, Advance Java Concepts, Java GUI (Swing
-            Framework), DSA and Algorithm. <br />
-            <br />I have solved many problems on HackerRank, HackerEarth website
-            in Java and earned 5-star bedge. <br />
-            <br />I also made many Projects on my own in Core Java, Java Swing
-            and etc.<span className="thank">:-Thank You for your Time</span>
+            I have a keen interest in Web Development, thus as a result I have
+            learnt HTML, CSS, JavaScript. <br />
+            <br />
+            To go on to next level, I have also learnt Libraries like ReactJS
+            with ViteJS. <br />
+            <br />I also some of Online Project on this site. That I have
+            developed to test my Knowledge
+            <span className="thank">:-Thank You for your Time</span>
           </p>
         </div>
       </div>
